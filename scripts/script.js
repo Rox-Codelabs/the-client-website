a8d1e4a75e8d3b0c1294171c42f9c822a5085ee6
Roxy <149881577+Rox-Codelabs@users.noreply.github.com>
2025-09-26 12:10:09 +0200

    #2 HTML structure

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,31 @@
 // JS
+export const items = [
+    {
+        name: "statestieken",
+        category: "concept"
+    }
+    {
+        name: "handhaver",
+        category: "concept"
+    }
+    {
+        name: "seintje",
+        category: "concept experiment"
+    }
+    {
+        name: "cameradoos",
+        category: "concept"
+    }
+    {
+        name: "maquette",
+        category: "concept experiment methode"
+    }
+    {
+        name: "waardenkaartjes",
+        category: "concept experiment methode"
+    }
+    {
+        name: "menselijke-maat",
+        category: "concept experiment methode"
+    }
+]
